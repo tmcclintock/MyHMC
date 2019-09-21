ea50cb78244fceb47570eca13ce85a69ff8f86a8
Tom McClintock <tmcclintock89@gmail.com>
2019-09-20 23:41:31 -0400

    Making the sampler.

--- MyHMC/MyHMC.py
+++ MyHMC/MyHMC.py
@@ -3,5 +3,16 @@
 import numpy as np
 
 class MyHMC(class):
-    def __init__(self, lnpost):
+    def __init__(self, lnpost, q0, Nsamples, M):
         self.lnpost = lnpost
+        self.q0 = q0
+        self.Nsamples = Nsamples
+        self.M = M
+        self.Minv = np.linalg.inv(M)
+
+        #Write a method to sample the momenta
+        #Make a leapfrogger
+        #Keep track of samples
+        #Loop over the leapfrogger passing in new samples and
+        #momenta every time
+        #Write a MH acceptance step
